c9e3e25c6f0d4f934793ae4f2973137e373c1a0f
ercntreras <er.contreras@hotmail.com>
2025-11-27 01:24:11 -0600

    Add spec when request GET api/v1/subjects and respond Json with a list of 5

--- spec/requests/subject_spec.rb
+++ spec/requests/subject_spec.rb
@@ -1,11 +1,22 @@
 require "rails_helper"
 
 Rails.describe "Api::V1::Subjects", type: :request do
+  let(:headers) { { "ACCEPT" => "application/json" } }
+  let(:json_response) { response.parsed_body }
+
   describe "Get /api/v1/subjects" do
+    let!(:subjects) { create_list(:subject, 5) }
+
     it "return a sucessful response" do
-      get "/api/v1/subjects", headers: { "ACCEPT" => "application/json" }
+      get "/api/v1/subjects", headers: headers
 
       expect(response).to have_http_status(:ok)
+    end
+
+    it "return a list of subjects" do
+      get "/api/v1/subjects", headers: headers
+
+      expect(json_response.count).to eq(5)
     end
   end
 end
