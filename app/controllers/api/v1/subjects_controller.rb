26101a38451ce8652f41a1660944956ce3375381
ercntreras <er.contreras@hotmail.com>
2025-11-30 01:23:49 -0600

    Add basic view templates

--- app/controllers/api/v1/subjects_controller.rb
+++ app/controllers/api/v1/subjects_controller.rb
@@ -1,13 +1,36 @@
 class Api::V1::SubjectsController < ApplicationController
   def index
-    subjects = Subject.all
+    @subjects = Subject.all
 
     respond_to do |format|
       format.html { render :index }
       format.json { render json: {
-        subjects: Panko::ArraySerializer.new(subjects, each_serializer: SubjectSerializer).to_a
+        subjects: Panko::ArraySerializer.new(@subjects, each_serializer: SubjectSerializer).to_a
         }, status: :ok
       }
     end
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to @subject
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :subjects, only: [ :index ]
+      resources :subjects
     end
   end
 
