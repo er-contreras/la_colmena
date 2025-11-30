cac9d674b3f454d21f6f6580c866be07a1044da7
ercntreras <er.contreras@hotmail.com>
2025-11-30 13:28:03 -0600

    Update subjects specs as it does not handle json responses

--- spec/requests/subject_spec.rb
+++ spec/requests/subject_spec.rb
@@ -1,21 +1,21 @@
 require "rails_helper"
 
 Rails.describe "Subjects", type: :request do
-  let(:json_response) { response.parsed_body }
-
   describe "Get /subjects" do
     let!(:subjects) { create_list(:subject, 5) }
 
     it "return a sucessful response" do
-      get "/subjects", as: :json
+      get "/subjects"
 
       expect(response).to have_http_status(:ok)
     end
 
-    it "return a list of subjects" do
-      get "/subjects", as: :json
+    it "render subjects titles" do
+      get "/subjects"
 
-      expect(json_response.count).to eq(5)
+      subjects.each do |subject|
+        expect(response.body).to include(subject.title)
+      end
     end
   end
 end
