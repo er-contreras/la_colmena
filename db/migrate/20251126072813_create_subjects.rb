0be89233a4a5957c09b66e6f29b5f3db11997809
ercntreras <er.contreras@hotmail.com>
2025-12-03 00:45:30 -0600

    Add subjects controller and views with first_turbo_frame_tag

--- db/migrate/20251126072813_create_subjects.rb
+++ db/migrate/20251126072813_create_subjects.rb
@@ -1,7 +1,8 @@
 class CreateSubjects < ActiveRecord::Migration[8.1]
   def change
     create_table :subjects do |t|
-      t.string :title
+      t.string :title, null: false
+      t.boolean :completed, default: false, null: false
 
       t.timestamps
     end
